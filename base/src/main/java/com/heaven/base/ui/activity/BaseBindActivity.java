6957e428ca42e4f12a3781a93d085aed38a22b05
heavenisme <heavenisme@aliyun.com>
2019-02-25 16:41:23 +0800

    build.gradle 调整

--- base/src/main/java/com/heaven/base/ui/activity/BaseBindActivity.java
+++ base/src/main/java/com/heaven/base/ui/activity/BaseBindActivity.java
@@ -2,13 +2,20 @@ package com.heaven.base.ui.activity;
 
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.View;
 
+import com.heaven.base.R;
 import com.heaven.base.presenter.BasePresenter;
 import com.heaven.base.presenter.IView;
+import com.heaven.base.ui.SpUtil;
+import com.heaven.base.ui.view.widget.SwipeBackLayout;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
@@ -27,19 +34,15 @@ public abstract class BaseBindActivity<P extends BasePresenter, VM extends BaseV
     public VM mViewModel;
     private P mPresenter;
 
-    private void initPresenter(Class pt) {
-
-    }
-
-    private void initViewModel(Class vm) {
-
-    }
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        View rootView = getLayoutInflater().inflate(this.initLayoutResId(), null, false);
+        mViewBinding = DataBindingUtil.bind(rootView);
+        this.makeContentView(rootView);
         init();
+        initView();
     }
 
     private void init() {
@@ -82,5 +85,28 @@ public abstract class BaseBindActivity<P extends BasePresenter, VM extends BaseV
             e.printStackTrace();
         }
         return null;
+    }
+
+
+    public void makeContentView(View rootView) {
+        super.setContentView(getContainer(rootView));
+    }
+
+    private View getContainer(View rootView) {
+        rootView.setBackgroundColor(ContextCompat.getColor(this, R.color.alpha_white));
+        View container = getLayoutInflater().inflate(R.layout.activity_base, null, false);
+        SwipeBackLayout swipeBackLayout = container.findViewById(R.id.swipeBackLayout);
+        swipeBackLayout.setDragEdge(SwipeBackLayout.DragEdge.LEFT);
+        View ivShadow = container.findViewById(R.id.iv_shadow);
+        swipeBackLayout.addView(rootView);
+        swipeBackLayout.setOnSwipeBackListener((fa, fs) -> ivShadow.setAlpha(1 - fs));
+        return container;
+    }
+
+
+    public void reload() {
+        AppCompatDelegate.setDefaultNightMode(SpUtil.isNight() ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO);
+        getWindow().setWindowAnimations(R.style.WindowAnimationFadeInOut);
+        recreate();
     }
 }
