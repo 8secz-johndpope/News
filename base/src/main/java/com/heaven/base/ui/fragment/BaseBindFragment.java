01dcc7cfecc4dfb59625961191190c4d7e2afeae
heavenisme <heavenisme@aliyun.com>
2019-02-26 10:23:32 +0800

    build.gradle 调整

---
+++ base/src/main/java/com/heaven/base/ui/fragment/BaseBindFragment.java
@@ -0,0 +1,105 @@
+package com.heaven.base.ui.fragment;
+
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.heaven.base.presenter.BasePresenter;
+import com.heaven.base.vm.BaseViewModel;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+/**
+ * FileName: com.heaven.base.ui.fragment.BaseBindFragment.java
+ * author: Heaven
+ * email: heavenisme@aliyun.com
+ * date: 2019-02-26 10:10
+ *
+ * @version V1.0 TODO <描述当前版本功能>
+ */
+@SuppressWarnings("unchecked")
+public abstract class BaseBindFragment<P extends BasePresenter, VM extends BaseViewModel<P>, B extends ViewDataBinding> extends Fragment {
+    protected B mViewBinding;
+    protected VM mViewModel;
+    private P mPresenter;
+    private BaseFragment.OnFragmentInteractionListener mListener;
+
+    /**
+     * 获取layout的id，具体由子类实现
+     *
+     * @return 布局id
+     */
+    protected abstract int initLayoutResId();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mViewBinding = DataBindingUtil.inflate(inflater,initLayoutResId(), container, false);
+        return mViewBinding.getRoot();
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof BaseFragment.OnFragmentInteractionListener) {
+            mListener = (BaseFragment.OnFragmentInteractionListener) context;
+        }
+    }
+
+    private void init() {
+        analyseGenerics();
+    }
+
+    /**
+     * 范型参数解析
+     */
+    private void analyseGenerics() {
+        Type type = this.getClass().getGenericSuperclass();
+        if (type instanceof ParameterizedType) {
+            Type[] typeArr = ((ParameterizedType) type).getActualTypeArguments();
+            if (typeArr.length > 0) {
+                for (Type clazzType : typeArr) {
+                    Class clazz = (Class) clazzType;
+                    String presentName = BasePresenter.class.getName();
+                    String baseViewModelName = BaseViewModel.class.getName();
+                    String clazzName = "";
+                    if (clazz.getSuperclass() != null) {
+                        clazzName = clazz.getSuperclass().getName();
+                    }
+
+                    if (clazzName.equals(presentName)) {
+                        mPresenter = getInstance(clazz);
+                    } else if (clazzName.equals(baseViewModelName)) {
+                        ViewModelProvider.AndroidViewModelFactory factory = ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication());
+                        mViewModel = (VM) ViewModelProviders.of(this, factory).get(clazz);
+                        mViewModel.setPresenter(mPresenter);
+                    }
+                }
+            }
+        }
+    }
+
+    protected P getInstance(Class clazz) {
+        try {
+            return (P) clazz.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ base/src/main/java/com/heaven/base/ui/fragment/BaseSimpleBindFragment.java
@@ -0,0 +1,92 @@
+package com.heaven.base.ui.fragment;
+
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.heaven.base.presenter.BasePresenter;
+import com.heaven.base.vm.BaseViewModel;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+/**
+ * FileName: com.heaven.base.ui.fragment.BaseBindFragment.java
+ * author: Heaven
+ * email: heavenisme@aliyun.com
+ * date: 2019-02-26 10:10
+ *
+ * @version V1.0 TODO <描述当前版本功能>
+ */
+@SuppressWarnings("unchecked")
+public abstract class BaseSimpleBindFragment<VM extends BaseViewModel, B extends ViewDataBinding> extends Fragment {
+    protected B mViewBinding;
+    protected VM mViewModel;
+    private BaseFragment.OnFragmentInteractionListener mListener;
+
+    /**
+     * 获取layout的id，具体由子类实现
+     *
+     * @return 布局id
+     */
+    protected abstract int initLayoutResId();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mViewBinding = DataBindingUtil.inflate(inflater,initLayoutResId(), container, false);
+        return mViewBinding.getRoot();
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof BaseFragment.OnFragmentInteractionListener) {
+            mListener = (BaseFragment.OnFragmentInteractionListener) context;
+        }
+    }
+
+    private void init() {
+        analyseGenerics();
+    }
+
+    /**
+     * 范型参数解析
+     */
+    private void analyseGenerics() {
+        Type type = this.getClass().getGenericSuperclass();
+        if (type instanceof ParameterizedType) {
+            Type[] typeArr = ((ParameterizedType) type).getActualTypeArguments();
+            if (typeArr.length > 0) {
+                for (Type clazzType : typeArr) {
+                    Class clazz = (Class) clazzType;
+                    String baseViewModelName = BaseViewModel.class.getName();
+                    String clazzName = "";
+                    if (clazz.getSuperclass() != null) {
+                        clazzName = clazz.getSuperclass().getName();
+                    }
+
+                    if (clazzName.equals(baseViewModelName)) {
+                        ViewModelProvider.AndroidViewModelFactory factory = ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication());
+                        mViewModel = (VM) ViewModelProviders.of(this, factory).get(clazz);
+                    }
+                }
+            }
+        }
+    }
+
+}
