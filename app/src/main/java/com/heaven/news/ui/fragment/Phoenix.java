6e5ba6e7a3bb067fba1c1b4c5f465f812d7cab70
heavenisme <heavenisme@aliyun.com>
2019-04-02 22:20:30 +0800

    架构调整

--- app/src/main/java/com/heaven/news/ui/fragment/Phoenix.java
+++ app/src/main/java/com/heaven/news/ui/fragment/Phoenix.java
@@ -1,12 +1,23 @@
 package com.heaven.news.ui.fragment;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
 
+import com.heaven.base.ui.adapter.BaseAdapter;
 import com.heaven.base.ui.fragment.BaseSimpleBindFragment;
+import com.heaven.base.ui.view.widget.banner.LoopRecyclerViewPager;
+import com.heaven.base.ui.view.widget.banner.RecyclerViewPagerListener;
 import com.heaven.news.R;
 import com.heaven.news.databinding.PhoenixBinding;
+import com.heaven.news.engine.AppEngine;
+import com.heaven.news.ui.vm.holder.HomeBanner;
+import com.heaven.news.ui.vm.model.base.HomeImageInfo;
+import com.heaven.news.ui.vm.model.base.ImageInfo;
 import com.heaven.news.ui.vm.viewmodel.MainViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * FileName: com.heaven.news.ui.fragment.Home.java
@@ -17,7 +28,7 @@ import com.heaven.news.ui.vm.viewmodel.MainViewModel;
  * @version V1.0 TODO <描述当前版本功能>
  */
 public class Phoenix extends BaseSimpleBindFragment<MainViewModel,PhoenixBinding>{
-
+    BaseAdapter<ImageInfo> mBannerAdapter;
     @Override
     public void bindModel() {
 
@@ -26,6 +37,46 @@ public class Phoenix extends BaseSimpleBindFragment<MainViewModel,PhoenixBinding
     @Override
     public int initLayoutResId() {
         return R.layout.phoenix;
+    }
+
+    @Override
+    public void initView(View rootView) {
+        super.initView(rootView);
+        initTopBanner();
+    }
+
+    private void initTopBanner() {
+        LoopRecyclerViewPager mRecyclerView = mViewBinding.imageViewPager;
+        LinearLayoutManager layout = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        mBannerAdapter = new BaseAdapter<>(getContext());
+        mBannerAdapter.register(new HomeBanner(ImageInfo.class, R.layout.easygo_banner_item));
+        mRecyclerView.setLayoutManager(layout);
+        mRecyclerView.setAdapter(mBannerAdapter);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLongClickable(true);
+        RecyclerViewPagerListener bannerListener = new RecyclerViewPagerListener(mRecyclerView);
+        mRecyclerView.addOnScrollListener(bannerListener);
+        mRecyclerView.addOnLayoutChangeListener(bannerListener);
+        updateHomeImageData();
+        mRecyclerView.setAutoLoop(true);
+        mRecyclerView.startLoop();
+    }
+
+    public void updateHomeImageData() {
+        HomeImageInfo homeImageInfo = AppEngine.instance().dataCore().getHomeConfigData();
+        List<ImageInfo> bannerList = new ArrayList<>();
+        if (mViewBinding != null) {
+            if (homeImageInfo != null) {
+                if (homeImageInfo.phoenix != null && homeImageInfo.phoenix.size() > 0) {
+                    bannerList = homeImageInfo.phoenix;
+                }
+            }
+            if(bannerList.size() == 0) {
+                bannerList.add(new ImageInfo());
+            }
+            mBannerAdapter.updateItems(bannerList);
+            mViewBinding.indicators.setViewPager(mViewBinding.imageViewPager);
+        }
     }
 
     @Override
